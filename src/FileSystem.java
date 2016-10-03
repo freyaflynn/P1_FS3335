db927e032ba952d462fffdf65692cb92619120ab
Freya Flynn <pflynn2@acad-jbws-10-4-66-141.stedwards.edu>
2016-10-03 03:11:39 -0500

    Various parts, unpolished
    hasFile, remFile, toString

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -53,13 +53,22 @@ public class FileSystem {
 			return false;
 		}
 		
+		int currBlock = -1;
+		int nextBlock;
 		for (int i = 0; i < MAX_BLOCKS; i++) {
-			
+			if (fileInfo[i].fileName.equalsIgnoreCase(fName)) {
+				currBlock = fileInfo[i].firstBlock;
+				fileInfo[i] = null;
+				break;
+			}
 		}
-		//Find file in fileInfo
-		//Get 1st block from fileInfo
-		//Clear fileInfo
-		//Get next block from current block (blockInfo), clear current blockInfo, go to next block, repeat
+		
+		while (blockInfo[currBlock] > 0) {
+			nextBlock = blockInfo[currBlock];
+			blockInfo[currBlock] = 0;
+			currBlock = nextBlock;
+		}
+		blockInfo[currBlock] = 0;
 		return true;
 	}
 	
@@ -78,12 +87,70 @@ public class FileSystem {
 	
 	public boolean hasFile (String fName) {
 		//look through fileInfo for fName
-		return true;
+		boolean hasFile = false;
+		for (int i = 0; i < MAX_BLOCKS; i++) {
+			if (fileInfo[i] != null && fName.equalsIgnoreCase(fileInfo[i].fileName)) {
+				hasFile = true;
+				break;
+			}
+		}
+		return hasFile;
 	}
 	
 	public String toString () {
 		//256 char boxes with block num and file name at the top, block order
-		return "";
+		String temp = "";
+		String border = "|-";
+		String [] blockToFile = new String [MAX_BLOCKS];
+		int height;
+		int width;
+		if (BLOCK_SIZE/8 > 60) {
+			width = 60;
+			height = BLOCK_SIZE/60;
+		} else {
+			height = 8;
+			width = BLOCK_SIZE/8;
+		}
+		
+		for (int i = 0; i < width; i++)
+			border += "-";
+		
+		
+		//Generates array blockToFile
+		for (int i = 0; i < MAX_BLOCKS; i++) {
+			if (fileInfo[i] != null) {
+				int currBlock = fileInfo[i].firstBlock;
+				while (blockInfo[currBlock] > 0) {
+					blockToFile[currBlock] = fileInfo[i].fileName;
+					currBlock = blockInfo[currBlock];
+				}
+				blockToFile[currBlock] = fileInfo[i].fileName;
+			}
+		}
+		
+		
+		
+		for (int i = 0; i < MAX_BLOCKS; i++) {
+			int fileLength = 0;
+			if (blockToFile[i] != null)
+					fileLength = blockToFile[i].length();
+			temp += border + "\n";
+			temp += "|Block:" + String.format("%1$04d", i);
+			for (i = 0; i < width-10-fileLength; i++) {
+				temp+= " ";
+			}
+			temp += blockToFile[i];
+			temp += "|\n";
+			temp += border + "\n";
+			String data = drive[i];
+			if (data == null)
+				data = "";
+			for (i = 0; i < data.length(); i += width) {
+				temp += "|" + data.substring(i, i+width-1) + "\n";
+			}
+			temp += "\n";
+		}
+		return temp;
 	}
 	
 	//Adds a new FileInfo object to the fileInfo array
@@ -91,7 +158,7 @@ public class FileSystem {
 		FileInfo fi = new FileInfo(fName, firstBlock);
 		if (hasFile(fName))
 			for (int i = 0; i < MAX_BLOCKS; i++) {
-				if (fileInfo[i].fileName.equalsIgnoreCase(fName)) {
+				if (fileInfo[i] != null && fileInfo[i].fileName.equalsIgnoreCase(fName)) {
 					fileInfo[i] = fi;
 					break;
 				}
--- src/TextUI.java
+++ src/TextUI.java
@@ -91,7 +91,15 @@ public class TextUI {
 				System.out.println(sysDialogue.get("case4") + "\n");
 				active = false;
 				break;
-				
+			
+			case 'z':
+				fName = input.next();
+				input.hasNext();
+				data = input.nextLine();
+				System.out.print("fName: " + fName + "  data: " + data);
+				FS3335.newFile(fName, data);
+				System.out.println(FS3335.toString());
+				break;
 			default:
 				System.out.println(sysDialogue.get("invalidcase") + "\n");
 				break;
@@ -106,6 +114,7 @@ public class TextUI {
 		Map<String,String> temp = new HashMap<String,String>();
 		try {
 			Scanner dialogue = new Scanner(new File("sysDialogue.txt"));
+			dialogue.useDelimiter("~~");
 			while (dialogue.hasNext()) {
 				temp.put(dialogue.next().trim(), dialogue.next().trim());
 				}
